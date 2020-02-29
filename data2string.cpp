0e7cbbbe33d86efe17b998d4c7bbd783da0e99bb
Harini Venkatesan <hvenk001@ucr.edu>
2020-02-28 20:30:34 -0800

    Update data2string.cpp

--- data2string.cpp
+++ data2string.cpp
@@ -21,3 +21,48 @@ int main(){
 
     return 0;
 }
+
+#include <opendht.h>
+#include <sstream>
+#include <string>
+
+dht::DhtRunner node;
+
+
+void foo() {
+	std::string aString = "1234abcd";
+	dht::Value valueString(aString);
+	
+	node.put("unique_key", aString);
+
+	sleep(1);
+}
+
+
+void bar() {
+		
+	node.get("unique_key", [](const std::vector<std::shared_ptr<dht::Value>>& values) {
+        // Callback called when values are found
+        for (const auto& value : values){
+		//dht::Value tempValue;
+		//std::cout << typeid((*value).data[0]).name() << std::endl; 
+		//tempValue = *value;
+		for(int i = 0; i < (*value).data.size(); i++){
+			
+			printf("%c\n",static_cast<char>(static_cast<int>((*value).data.at(i))));
+		}
+	}
+        return true; // return false to stop the search
+    	});
+	sleep(1);
+}
+
+int main() {
+	node.run(4222, dht::crypto::generateIdentity(), true);
+	node.bootstrap("bootstrap.jami.net", "4222");
+
+	foo();
+	bar();
+	return 0;
+
+}
