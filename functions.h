a9674b644b41e7c4e7f00bd3e6ff141f01dd3cc7
Eric Chan <echan044@ucr.edu>
2020-03-04 17:22:30 -0800

    auxiliary functions

---
+++ functions.h
@@ -0,0 +1,39 @@
+/* Create a unique number for each file */
+unsigned long long idGenerator() {
+        std::random_device rd;
+        std::default_random_engine generator(rd());
+        /* 1 is reserved for the root */
+        std::uniform_int_distribution<long long unsigned> distribution(2,0xFFFFFFFFFFFFFFFF);
+
+        return distribution(generator);
+}
+
+/* Find parent path given a path */
+std::string parent_path(std::string path_name) {
+        std::string parent_path = path_name.substr(0, path_name.find_last_of("/"));
+
+        /* If empty, parent is the root */
+        if (parent_path == "") {
+                parent_path = "/";
+        }
+        return parent_path;
+}
+
+/* Find file name given full path */
+std::string file_name(std::string path_name) {
+        std::string file_name = path_name.substr(path_name.find_last_of("/") + 1);
+
+        return file_name;
+}
+
+/* Converts vector to ASCII string */
+std::string asciify(std::vector<auto> const &vec) {
+    std::string fullString;
+
+    for (int i = 1; i < vec.size(); i++) {
+        fullString += static_cast<char>(vec[i]);
+    }
+
+    return fullString;
+}
+
