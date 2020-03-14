a18e69a19688cb7301d2562f15ba74c154250f16
eric-m-chan <echan044@ucr.edu>
2020-03-14 14:33:44 -0700

    Add prototype of write and rename functions
    - write only appends content
    - rename only works with FILES, not directories

--- dht_functions.h
+++ dht_functions.h
@@ -23,11 +23,10 @@ void dht_init_root() {
 }
 
 void dht_create(std::string path_name) {
+	std::cout << "=== dht_create(" << path_name << ") ===\n";
 	if (!file_exists(path_name)) {
 		unsigned long long file_id = idGenerator();
 		std::string parent_uuid;
-
-		std::cout << "=== dht_create(" << path_name << ") ===\n";
 
 		/* put new file into parent path */
 		parent_uuid = parent_path(path_name);
@@ -50,7 +49,7 @@ void dht_create(std::string path_name) {
 			/* put <path, id> */
 			dht_exclusive_put(path_name, std::to_string(file_id), 0, 0);
 			/* put <id, <id, "", 1, 0>> */
-			new_file.content = "";
+			new_file.content = "hello";
 			dht_exclusive_put(std::to_string(file_id), parseOutgoingDHT(new_file), 0, 1);
 		}
 		else {
@@ -101,8 +100,7 @@ void dht_mkdir(std::string path_name) {
 	std::cout << "=== dht_mkdir complete ===\n\n";
 }
 
-/* remove file at path_name 
-   idea: put the same thing with isExist = 0 */
+/* remove file at path_name - put the same thing with isExist = 0 */
 void dht_remove(std::string path_name) {
 	std::cout << "=== dht_remove(" << path_name << ") ===\n";
 	if (file_exists(path_name)) {
@@ -155,6 +153,46 @@ void dht_remove(std::string path_name) {
 	std::cout << "=== dht_remove complete ===\n\n";
 }
 
+/* renames path_from to path_to */
+void dht_rename(std::string path_from, std::string path_to) {
+	std::cout << "=== dht_rename(" << path_from << ") ===\n";
+	if (file_exists(path_from)) {
+		if (!file_exists(path_to)) {
+			if(is_directory(path_from)) {
+				/* TODO renaming a directory */
+
+			}
+			else {
+				/* renaming a file */
+				std::cout << "renaming FILE '" << path_from << "' to '" << path_to << "'...\n";
+				std::string path_from_uuid = existing_uuid_key(path_from);
+				std::string parent_uuid = existing_uuid_key(parent_path(path_from));
+				
+				/* file with format to push it to parent */
+				file child_file(std::stoull(path_from_uuid), file_name(path_to), 1, 0);
+				/* remove <path_from, uuid> entry and <parent_from, <...>> */
+				dht_remove(path_from);
+				/* put <path_to, uuid of path_from> */
+				node.put(path_to, path_from_uuid);
+				/* put name of new file in parent */
+				node.put(parent_uuid, parseOutgoingDHT(child_file));
+				//TODO xcp maybe?
+				//dht_exclusive_put(path_to, path_uuid, 0, 0);
+				//dht_exclusive_put(parent_uuid, file_name(path_to), 0, 0);
+			}
+		}
+		else {
+			/* dst file already exists */
+			std::cout << "ERROR2: rename: cannot rename '" << path_to << "': already exists\n";
+		}
+	}
+	else {
+		/* src file does not exist */
+		std::cout << "ERROR1: rename: cannot rename '" << path_from << "': is not a file or directory\n";
+	}
+	std::cout << "=== dht_rename complete ===\n\n";
+}	
+
 /* return the contents of a file (path_name) */
 std::string dht_read(std::string path_name) {
 	std::cout << "=== dht_read(" << path_name << ") ===\n";
@@ -162,21 +200,54 @@ std::string dht_read(std::string path_name) {
 		if(!is_directory(path_name)) {
 			/* string to hold uuid */
 			std::string path_uuid;
-			/* file in string format */
-			std::string fileString;
+			/* vector that stores all file strings */
+			std::vector<std::string> fileStringsVec;
+			/* vector that stores all files */
+			std::vector<file> fileVec;
+			/* vector that stores all (labeled) deleted files */
+			std::vector<file> deletedFilesVec;
+                	/* vector that will filter out to only existingFilesVec */
+			std::vector<file> filterFilesVec;
 			file current_file;
 
 			path_uuid = existing_uuid_key(path_name);
-			node.get(path_uuid, [&fileString](const std::vector<std::shared_ptr<dht::Value>>& values) {
+			node.get(path_uuid, [&fileStringsVec](const std::vector<std::shared_ptr<dht::Value>>& values) {
 				for (const auto& value : values) {
-					//should only be one value
-					fileString = asciify((*value).data);
+					std::string uuid2file = asciify((*value).data);
+					fileStringsVec.push_back(uuid2file);
 				}
 				return true;
 			});
 			sleep(1);
 
-			current_file = parseIncomingDHT(fileString);
+			for (int i = 0; i < fileStringsVec.size(); i++) {
+				fileVec.push_back(parseIncomingDHT(fileStringsVec[i]));
+			}
+
+			for (int i = 0; i < fileVec.size(); i++) {
+				/* populate vector of deleted files */
+				if (fileVec[i].isExists == false) {
+					deletedFilesVec.push_back(fileVec[i]);
+				} 
+				else {
+					filterFilesVec.push_back(fileVec[i]);
+				}
+			}
+			
+			/* find the file that has not been deleted */
+			for (int i = 0; i < filterFilesVec.size(); i++) {
+				bool fileExists = true;
+				for (int j = 0; j < deletedFilesVec.size(); j++) {
+					if (filterFilesVec[i] == deletedFilesVec[j]) {
+						fileExists = false;
+					}
+				}
+				if (fileExists) {
+					current_file = filterFilesVec[i];
+					break;
+				}
+			}
+			
 			std::cout << "=== dht_read complete ===\n\n";
 			return current_file.content;
 		}
@@ -192,6 +263,54 @@ std::string dht_read(std::string path_name) {
 		std::string emptyString;
 		std::cout << "=== dht_read complete ===\n\n";
 		return emptyString;
+	}
+}
+
+/* append content to a file */
+void dht_write(std::string path_name, std::string content) {
+	std::cout << "=== dht_write(" << path_name << ") ===\n";
+	if (file_exists(path_name)) {
+		if (!is_directory(path_name)) {	
+			/* string that holds the uuid of the file */
+			std::string path_uuid;
+			/* file in string format */
+			std::string fileString;
+			file current_file;
+			file delete_file;
+
+			std::cout << "Writing '"<< content << "' to " << file_name(path_name) << "...\n";
+			
+			//TODO deleted values
+			path_uuid = existing_uuid_key(path_name);
+			node.get(path_uuid, [&fileString](const std::vector<std::shared_ptr<dht::Value>>& values) {
+				for (const auto& value : values) {
+					//should only be one value
+					fileString = asciify((*value).data);
+					}
+					return true;
+			});
+			sleep(1);
+
+			delete_file = parseIncomingDHT(fileString);
+			current_file = parseIncomingDHT(fileString);
+
+			/* delete the old file */
+			delete_file.isExists = false;
+			node.put(path_uuid, parseOutgoingDHT(delete_file));
+
+			/* append the new content */
+			current_file.content = current_file.content + content;
+			/* put the new file */
+			node.put(path_uuid, parseOutgoingDHT(current_file));
+
+			std::cout << "=== dht_write complete ===\n\n";
+		}
+		else {
+			std::cout << "ERROR2: write: cannot write to '" << path_name << "file is a directory\n";
+		}
+	}
+	else {
+		std::cout << "ERROR1: write: cannot write to '" << path_name << "file does not exist\n";
 	}
 }
 
@@ -268,12 +387,7 @@ std::vector<std::string> dht_readdir(std::string path_name) {
 	}
 	else {
 		std::cout << "ERROR1: readdir: cannot readdir '" << path_name << "': is not a directory\n";
-		if (file_exists(path_name)) {
-			std::cout << "AT LEAST I PASSED FILE EXISTS\n";
-		}
-		else {
-			std::cout << "I FAILED FILE EXISTS TOO\n";
-		}
+		
 		std::vector<std::string> emptyVec;
 		std::cout << "=== dht_readdir complete ===\n\n";
 		return emptyVec;
@@ -285,6 +399,28 @@ int main() {
         node.bootstrap("bootstrap.jami.net", "4222");
 	
 	/* for test purposes */
+
+
+	/* testing rename on a FILE... */
+	dht_init_root();
+	dht_mkdir("/A");
+	dht_create("/A/hello.txt");
+	dht_readdir("/A");
+	dht_rename("/A/hello.txt", "/A/world.txt");
+	dht_readdir("/A");
+
+	/* //testing read and write... 
+	dht_init_root();
+	dht_mkdir("/A");
+	dht_create("/A/hello.txt");
+	std::string AString = dht_read("/A/hello.txt");
+	std::cout << "Astring: " << AString << std::endl;
+	dht_write("/A/hello.txt", " how are you?");
+	std::string BString = dht_read("/A/hello.txt");
+	std::cout << "Bstring: " << BString << std::endl;
+	*/
+
+	/* //testing remove... 
 	dht_init_root();
 	dht_mkdir("/Z");
 	dht_readdir("/");
@@ -292,10 +428,12 @@ int main() {
 	dht_create("/Z/hello.txt");
 	dht_readdir("/");
 	dht_mkdir("/Z");
-	dht_create("Z/world.txt");
+	dht_create("/Z/world.txt");
 	dht_readdir("/");
 	dht_readdir("/Z");
-	/*
+	*/
+	
+	/* //general testing...
 	dht_mkdir("/A");
 	dht_mkdir("/B");
 	dht_readdir("/");
